afa8896c01cfbcd3d5f57e232357abf10ff902a7
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2022-10-02 12:50:08 -0400

    cleaned up menu code and added tabs

--- src/main/java/mostlyfireproof/konpeko/ui/MenuGui.java
+++ src/main/java/mostlyfireproof/konpeko/ui/MenuGui.java
@@ -16,18 +16,36 @@ import net.minecraft.text.TranslatableTextContent;
 import net.minecraft.util.Identifier;
 
 public class MenuGui extends LightweightGuiDescription {
+    private int defaultWidth = 256;
+    private int defaultHeight = 100;
     public MenuGui() {
-        WGridPanel root = new WGridPanel();
-        setRootPanel(root);
-        root.setSize(256, 240);
-        root.setInsets(Insets.ROOT_PANEL);
+        // Setup:
+        WTabPanel tabs = new WTabPanel();
+        setRootPanel(tabs);
 
-        //WSprite icon = new WSprite(new Identifier("minecraft:textures/item/golden_carrot.png"));
-        //root.add(icon, 10, 10, 1, 1);
+        WGridPanel combat = new WGridPanel();
+        combat.setSize(defaultWidth, defaultHeight);
+        combat.setInsets(Insets.ROOT_PANEL);
 
-        WLabel label = new WLabel(Text.literal("Konpeko Menu"), 0x3f3f3f); // 0x3f3f3f
-        root.add(label, 5, 0, 2, 1);
+        WGridPanel util = new WGridPanel();
+        util.setSize(defaultWidth, defaultHeight);
+        util.setInsets(Insets.ROOT_PANEL);
 
+        WGridPanel misc = new WGridPanel();
+        misc.setSize(defaultWidth, defaultHeight);
+        misc.setInsets(Insets.ROOT_PANEL);
+
+        WGridPanel info = new WGridPanel();
+        info.setSize(defaultWidth, defaultHeight);
+        info.setInsets(Insets.ROOT_PANEL);
+
+        tabs.add(combat, tab -> tab.title(Text.literal("Combat")));
+        tabs.add(util, tab -> tab.title(Text.literal("Util")));
+        tabs.add(misc, tab -> tab.title(Text.literal("Misc")));
+        tabs.add(info, tab -> tab.title(Text.literal("Info")));
+
+
+        // Combat Menu:
         WToggleButton toggleButtonGlow = new WToggleButton(Text.literal("Toggle Glow"));
         toggleButtonGlow.setOnToggle(on -> {
             if (on) {
@@ -37,7 +55,7 @@ public class MenuGui extends LightweightGuiDescription {
             }
             Konpeko.LOGGER.info("Glow toggled to " + (on ? "on" : "off"));
         });
-        root.add(toggleButtonGlow, 0, 2, 10, 1);
+        combat.add(toggleButtonGlow, 0, 0, 10, 1);
 
         WToggleButton toggleAntiAntiCheat = new WToggleButton(Text.literal("Toggle AC Avoidance"));
         toggleAntiAntiCheat.setOnToggle(on -> {
@@ -48,18 +66,37 @@ public class MenuGui extends LightweightGuiDescription {
             }
             Konpeko.LOGGER.info("AntiAntiCheat toggled to " + (on ? "on" : "off"));
         });
-        root.add(toggleAntiAntiCheat, 0, 3, 10, 1);
+        combat.add(toggleAntiAntiCheat, 0, 1, 10, 1);
 
+
+        // Utility Menu:
+        // ideas: name scrambler, freecam
+
+        // Misc. Menu:
         WToggleButton toggleButtonRolex = new WToggleButton(Text.literal("Force Rolex"));
         toggleButtonRolex.setOnToggle(on -> {
             Konpeko.LOGGER.info("Rolex toggled to " + (on ? "on" : "off"));
         });
         //toggleButtonRolex.setOnImage(new Texture(new Identifier("minecraft:textures/item/redstone.png")));
+        misc.add(toggleButtonRolex, 0, 0);
 
-        root.add(toggleButtonRolex, 0, 6, 10, 1);
+
+        // Info Menu:
+        WLabel title = new WLabel(Text.literal("Konpeko"), 0x3f3f3f); // 0x3f3f3f
+        info.add(title, 5, 0, 2, 1);
+        WLabel author = new WLabel(Text.literal("A Minecraft utility mod by mostlyfireproof"));
+        info.add(author, 0, 1);
+
+        //WSprite icon = new WSprite(new Identifier("minecraft:textures/item/golden_carrot.png"));
+        //root.add(icon, 10, 10, 1, 1);
 
 
-        root.validate(this);
+        // Validation:
+        combat.validate(this);
+        util.validate(this);
+        misc.validate(this);
+        info.validate(this);
+        tabs.validate(this);
 
     }
 }
